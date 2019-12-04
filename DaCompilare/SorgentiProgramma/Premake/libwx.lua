0effb2daf369634558156898b6285d32672ed369
Sala Carlo <info@carlosala.net>
2019-12-04 19:23:41 +0100

    Update Signed-off-by: Sala Carlo <info@carlosala.net>

--- DaCompilare/SorgentiProgramma/Premake/libwx.lua
+++ DaCompilare/SorgentiProgramma/Premake/libwx.lua
@@ -1,9 +1,10 @@
+if (sq1 ~= "n") then
 filter { "configurations:Release*", "platforms:*" }
 	linkoptions { s1 }
 	links
 	{
-	--"sqlite3u",
-	--"wxsqlite3_msw" ..VER .. "u",
+	"sqlite3u",
+	"wxsqlite3_msw" ..VER .. "u",
 	"wxbase" .. VER .. "u",
 	"wxbase" .. VER .. "u_net",
 	"wxbase" .. VER .. "u_xml",
@@ -27,13 +28,42 @@ filter { "configurations:Release*", "platforms:*" }
 	"wxtiff",
 	"wxzlib"
 	}
-	
-	filter { "configurations:Debug*", "platforms:*" }
+else
+filter { "configurations:Release*", "platforms:*" }
 	linkoptions { s1 }
 	links
 	{
-	--"sqlite3ud",
-	--"wxsqlite3_msw" ..VER .. "ud",
+	"wxbase" .. VER .. "u",
+	"wxbase" .. VER .. "u_net",
+	"wxbase" .. VER .. "u_xml",
+	"wxexpat",
+	"wxjpeg",
+	"wxmsw" .. VER .. "u_adv",
+	"wxmsw" .. VER .. "u_core",
+	"wxmsw" .. VER .. "u_aui",
+	"wxmsw" .. VER .. "u_gl",
+	"wxmsw" .. VER .. "u_html",
+	"wxmsw" .. VER .. "u_media",
+	"wxmsw" .. VER .. "u_propgrid",
+	"wxmsw" .. VER .. "u_ribbon",
+	"wxmsw" .. VER .. "u_richtext",
+	"wxmsw" .. VER .. "u_stc",
+	"wxmsw" .. VER .. "u_webview",
+	"wxmsw" .. VER .. "u_xrc",
+	"wxpng",
+	"wxregexu",
+	"wxscintilla",
+	"wxtiff",
+	"wxzlib"
+	}
+	end
+if (sq1 ~= "n") then
+filter { "configurations:Debug*", "platforms:*" }
+	linkoptions { s1 }
+	links
+	{
+	"sqlite3ud",
+	"wxsqlite3_msw" ..VER .. "ud",
 	"wxmsw" .. VER .. "ud_core",
 	"wxbase" .. VER .. "ud",
 	"wxbase" .. VER .. "ud_net",
@@ -57,8 +87,36 @@ filter { "configurations:Release*", "platforms:*" }
 	"wxtiffd",
 	"wxzlibd"
 	}
-
-	filter { "configurations:*", "platforms:*" }
+else
+filter { "configurations:Debug*", "platforms:*" }
+	linkoptions { s1 }
+	links
+	{
+	"wxmsw" .. VER .. "ud_core",
+	"wxbase" .. VER .. "ud",
+	"wxbase" .. VER .. "ud_net",
+	"wxbase" .. VER .. "ud_xml",
+	"wxexpatd",
+	"wxjpegd",
+	"wxmsw" .. VER .. "ud_adv",
+	"wxmsw" .. VER .. "ud_aui",
+	"wxmsw" .. VER .. "ud_gl",
+	"wxmsw" .. VER .. "ud_html",
+	"wxmsw" .. VER .. "ud_media",
+	"wxmsw" .. VER .. "ud_propgrid",
+	"wxmsw" .. VER .. "ud_ribbon",
+	"wxmsw" .. VER .. "ud_richtext",
+	"wxmsw" .. VER .. "ud_stc",
+	"wxmsw" .. VER .. "ud_webview",
+	"wxmsw" .. VER .. "ud_xrc",
+	"wxpngd",
+	"wxregexud",
+	"wxscintillad",
+	"wxtiffd",
+	"wxzlibd"
+	}
+end	
+filter { "configurations:*", "platforms:*" }
 	links
 	{
 	"kernel32", "user32", "gdi32", "comdlg32", "winspool", "winmm", "shell32", "shlwapi", "comctl32", "ole32", 
--- DaCompilare/SorgentiProgramma/Premake/premake5.lua
+++ DaCompilare/SorgentiProgramma/Premake/premake5.lua
@@ -1,8 +1,12 @@
 local ROOT = "../"
--------------- wxWidgets o n --wxSQlite3 o n-------------
-local wx1 = "C:/wxWidgets-3.1.x" -- cambiare se si usa un'altra versione
-local sq1 = "n" -- wxSQlite3
-VER = "31" -- cambiare se si usa un'altra versione
+-------------- wxWidgets o n ---------------
+local wx1 = "C:/wxWidgets-3.1.x"
+-------- versione wxWidgets-----------------
+VER = "31"
+-------------- wxsqlite o n ----------------
+sq1 = "n"
+--------------------------------------------
+local nprog = "$(NOMPROG)"
 local STA32= "gcc_lib"
 local STA64= "gcc_x64_lib"
 local DIN32= "gcc_dll"
@@ -13,22 +17,19 @@ s2 = "-Wl,--end-group"
 local allegro = "n" -- o "n"
 --------------------------------------------
 workspace "MyWorkspace"
-   configurations { "DebugDinamic", "ReleaseDinamic", "DebugStatic", "ReleaseStatic" }
+	configurations { "DebugDinamic", "ReleaseDinamic", "DebugStatic", "ReleaseStatic" }
 	platforms      { "x64", "x32" }
-	project "Programma"
+	project ("Programma")
    kind "WindowedApp"
-   targetname ("$(NOMPROG)")
+   targetname (nprog)
    language "C++"
    --------------------------------------
     -- [ PROJECT FILES CONFIGURATIONS ] --
     --------------------------------------
     local SourceDir = ROOT .. "/"
     -- what files the visual studio project/makefile/etc should know about
-    files
+   	files
     { 
-      -- all paths in premake can have * for wildcard.
-      --     /Some/Path/*.txt     will find any .txt file in /Some/Path
-      --     /Some/Path/**.txt    will find any .txt file in /Some/Path and any of its subdirectories
       SourceDir .. "*.h", 
       SourceDir .. "*.hpp", 
       SourceDir .. "*.c",
@@ -40,180 +41,260 @@ workspace "MyWorkspace"
 	  defines { "D__WXDEBUG__", "HAVE_W32API_H", "__WXMSW__", "_UNICODE", "DWXUSINGDLL" }
       symbols "On"
 	  architecture "x86"
-	  if not (wx1=="n") then
+if (wx1 ~= "n") then
+if (sq1 ~= "n") then
 	  includedirs
     {
      wx1 .. "/include",
 	 wx1 .. "/lib/" .. DIN32 .."/mswud",
-	 --sq1 .. "/include"
-	 
+	 sq1 ..	"/include"
 	}
 	libdirs
     {
      wx1 .. "/lib/" .. DIN32 .."",
-	 --sq1 .. "/lib/" .. DIN32 ..""
-	 
-    }  
+	 sq1 .. "/lib/" .. DIN32 ..""
+    }
+else
+	  includedirs
+    {
+     wx1 .. "/include",
+	 wx1 .. "/lib/" .. DIN32 .."/mswud",
+	}
+	libdirs
+    {
+     wx1 .. "/lib/" .. DIN32 .."",
+    }
+end
    dofile "libwx.lua"
-   end
-    buildoptions { "-O0", "-Wall", "-std=gnu++11" }
-    
+end
+    buildoptions { "-O0", "-Wall", "-std=c++11" }
+    filter {}
 	filter { "configurations:DebugDinamic", "platforms:*64" }
  	  defines { "D__WXDEBUG__", "HAVE_W32API_H", "__WXMSW__", "_UNICODE", "DWXUSINGDLL" }
       symbols "On"
 	  architecture "x64"
-	  if not (wx1=="n") then
+if (wx1 ~= "n") then
+if (sq1 ~= "n") then
 	  includedirs
     {
      wx1 .. "/include",
 	 wx1 .. "/lib/" .. DIN64 .."/mswud",
-	 --sq1 .. "/include"
-	 }
+	 sq1 .. "/include"
+	}
 	libdirs
     {
      wx1 .. "/lib/" .. DIN64 .."",
-	 --sq1 .. "/lib/" .. DIN64 ..""
-	 
-    }  
+	 sq1 .. "/lib/" .. DIN64 ..""
+    }
+else
+	  includedirs
+    {
+     wx1 .. "/include",
+	 wx1 .. "/lib/" .. DIN64 .."/mswud",
+	}
+	libdirs
+    {
+     wx1 .. "/lib/" .. DIN64 .."",
+    }
+end
    dofile "libwx.lua"
-   end
-    buildoptions {"-O0", "-Wall", "-std=gnu++11" }
-	         
+end
+    buildoptions {"-O0", "-Wall", "-std=c++11" }
+	filter {}         
    filter { "configurations:ReleaseDinamic", "platforms:*32" }
 	  resoptions { "-F pe-i386" }
 	  defines { "NDEBUG", "HAVE_W32API_H", "__WXMSW__", "_UNICODE", "DWXUSINGDLL" }
       optimize "On"
 	  architecture "x86"
-     if not (wx1=="n") then
+if (wx1 ~= "n") then
+if (sq1 ~= "n") then
 	 includedirs
     {
      wx1 .. "/include",
 	 wx1 .. "/lib/" .. DIN32 .."/mswu",
-	 --sq1 .. "/include"
-	 
-	}
-	libdirs
+	 sq1 .. "/include"
+	 }
+	 libdirs
     {
      wx1 .. "/lib/" .. DIN32 .."",
-	 --sq1 .. "/lib/" .. DIN32 ..""
-	 
-    }  
-   dofile "libwx.lua"
-   end
-    buildoptions {"-Wall", "-std=gnu++11" }
+	 sq1 .. "/lib/" .. DIN32 ..""
+	 }
+else
+	 includedirs
+    {
+     wx1 .. "/include",
+	 wx1 .. "/lib/" .. DIN32 .."/mswu",
+	 }
+	 libdirs
+    {
+     wx1 .. "/lib/" .. DIN32 .."",
+	 }
+end	 
+	dofile "libwx.lua"
+end	
+   buildoptions {"-Wall", "-std=c++11" }
+   filter {}
    filter { "configurations:ReleaseDinamic", "platforms:*64" }
 	  defines { "NDEBUG", "HAVE_W32API_H", "__WXMSW__", "_UNICODE", "DWXUSINGDLL" }
       optimize "On"
-	  architecture "x64" 
-	  if not (wx1=="n") then
+	  architecture "x64"
+if (wx1 ~= "n") then
+if (sq1 ~= "n") then
 	  includedirs
       {
      wx1 .. "/include",
 	 wx1 .. "/lib/" .. DIN64 .."/mswu",
-	 --sq1 .. "/include"
-	 
+	 sq1 .. "/include"
 	}
 	libdirs
     {
      wx1 .. "/lib/" .. DIN64 .."",
-	 --sq1 .. "/lib/" .. DIN64 ..""
-	 
-    }  
+	 sq1 .. "/lib/" .. DIN64 ..""
+    }
+else	
+	  includedirs
+      {
+     wx1 .. "/include",
+	 wx1 .. "/lib/" .. DIN64 .."/mswu",
+	 }
+	libdirs
+    {
+     wx1 .. "/lib/" .. DIN64 .."",
+	}
+end
    dofile "libwx.lua"
-   end
-   	buildoptions { "-Wall", "-std=gnu++11" }
-	
+end
+   	buildoptions { "-Wall", "-std=c++11" }
+	filter {}
 	filter { "configurations:DebugStatic", "platforms:*32" }
 	  resoptions { "-F pe-i386" }
 	  defines { "D__WXDEBUG__", "HAVE_W32API_H", "__WXMSW__", "_UNICODE"}
       symbols "On"
 	  architecture "x86"
-	 if not (wx1=="n") then
+if (wx1 ~= "n") then
+if (sq1 ~= "n") then
 	 includedirs
     {
      wx1 .. "/include",
 	 wx1 .. "/lib/" .. STA32 .."/mswud",
-	 --sq1 .. "/include"
-	 
+	 sq1 .. "/include"
 	}
 	libdirs
     {
      wx1 .. "/lib/" .. STA32 .."",
-	 --sq1 .. "/lib/" .. STA32 ..""
-	 
-    }  
+	 sq1 .. "/lib/" .. STA32 ..""
+    }
+else
+	 includedirs
+    {
+     wx1 .. "/include",
+	 wx1 .. "/lib/" .. STA32 .."/mswud",
+	}
+	libdirs
+    {
+     wx1 .. "/lib/" .. STA32 .."",
+    }
+end	
    dofile "libwx.lua"
-   end
-    buildoptions { "-O0", "-Wall", "-std=gnu++11" }
-    
+end
+    buildoptions { "-O0", "-Wall", "-std=c++11" }
+    filter {}
 	filter { "configurations:DebugStatic", "platforms:*64" }
  	  defines { "D__WXDEBUG__", "HAVE_W32API_H", "__WXMSW__", "_UNICODE" }
       symbols "On"
 	  architecture "x64"
-	  if not (wx1=="n") then
+if (wx1 ~= "n") then
+if (sq1 ~= "n") then
 	  includedirs
     {
      wx1 .. "/include",
 	 wx1 .. "/lib/" .. STA64 .."/mswud",
-	 --sq1 .. "/include"
-	 
+	 sq1 .. "/include"
 	}
 	libdirs
     {
      wx1 .. "/lib/" .. STA64 .."",
-	 --sq1 .. "/lib/" .. STA64 ..""
-	 
-    }  
+	 sq1 .. "/lib/" .. STA64 ..""
+    }
+else
+	  includedirs
+    {
+     wx1 .. "/include",
+	 wx1 .. "/lib/" .. STA64 .."/mswud",
+	}
+	libdirs
+    {
+     wx1 .. "/lib/" .. STA64 .."",
+    }
+end	
    dofile "libwx.lua"
-   end
-    buildoptions { "-O0", "-Wall", "-std=gnu++11" }
-	         
+end
+    buildoptions { "-O0", "-Wall", "-std=c++11" }
+	filter {}         
    filter { "configurations:ReleaseStatic", "platforms:*32" }
 	  resoptions { "-F pe-i386" }
 	  defines { "NDEBUG", "HAVE_W32API_H", "__WXMSW__", "_UNICODE" }
       optimize "On"
 	  architecture "x86"
-	  if not (wx1=="n") then
+if (wx1 ~= "n") then
+if (sq1 ~= "n") then
 	  includedirs
     {
      wx1 .. "/include",
 	 wx1 .. "/lib/" .. STA32 .."/mswu",
-	 --sq1 .. "/include"
-	 }
+	 sq1 .. "/include"
+	}
 	
 	libdirs
     {
      wx1 .. "/lib/" .. STA32 .."",
-	 --sq1 .. "/lib/" .. STA32 ..""
-	 
+	 sq1 .. "/lib/" .. STA32 ..""
 	}  
-   
+else
+	  includedirs
+    {
+     wx1 .. "/include",
+	 wx1 .. "/lib/" .. STA32 .."/mswu",
+	}
+	
+	libdirs
+    {
+     wx1 .. "/lib/" .. STA32 .."",
+	}
+end	
    dofile "libwx.lua"
-   
-   end
-   
-   buildoptions { "-Wall", "-std=gnu++11" }
-    
+end
+   buildoptions { "-Wall", "-std=c++11" }
+   filter {} 
    filter { "configurations:ReleaseStatic", "platforms:*64" }
 	  defines { "NDEBUG", "HAVE_W32API_H", "__WXMSW__", "_UNICODE" }
       optimize "On"
 	  architecture "x64" 
-	  if not (wx1=="n") then
+if (wx1 ~= "n") then
+if (sq1 ~= "n") then
 	  includedirs
       {
      wx1 .. "/include",
 	 wx1 .. "/lib/" .. STA64 .."/mswu",
-	 --sq1 .. "/include"
-	 
+	 sq1 .. "/include"
 	}
 	libdirs
     {
      wx1 .. "/lib/" .. STA64 .."",
-	 --sq1 .. "/lib/" .. STA64 ..""
-	 
-    }  
+	 sq1 .. "/lib/" .. STA64 ..""
+    }
+else
+	  includedirs
+      {
+     wx1 .. "/include",
+	 wx1 .. "/lib/" .. STA64 .."/mswu",
+	}
+	libdirs
+    {
+     wx1 .. "/lib/" .. STA64 .."",
+    }
+end	
    dofile "libwx.lua"
-   end
-	buildoptions { "-Wall", "-std=gnu++11" }
-		   
+end
+	buildoptions { "-Wall", "-std=c++11" }
    filter {}
